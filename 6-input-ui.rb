b9cc1835009b09f4d4a95160d39dfc5da449d6c2
mfirmanakbar <m.firman.aa@gmail.com>
2019-05-16 09:53:05 +0700

    simple calculator 01:08

---
+++ 6-input-ui.rb
@@ -0,0 +1,7 @@
+puts "\nWhat is your name ?"
+name = gets
+puts ("Hello " + name + ", I'm Nic, nice to meet you.")
+
+puts "\nWhat is your favorite food ? "
+food = gets.chomp() #to remove enter that create break line
+puts ("I love " + food + ", it's delicious.\n\n")
---
+++ 7-calculator.rb
@@ -0,0 +1,6 @@
+puts "Enter a number"
+num1 = gets.chomp().to_i #need to convert to integer first
+puts "Enter another number"
+num2 = gets.chomp().to_i #need to convert to integer first
+
+puts (num1 + num2)
